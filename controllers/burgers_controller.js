c908c83d2cf5372ce2626d26196c17108940c2f2
Gregory Monroe II <gregthemusician@gmail.com>
2020-07-14 22:55:42 -0400

    changed selectAll function to all in burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  burger.all (function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -28,7 +28,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.updateOne({
+  burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
